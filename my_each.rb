4532416e3e3fd1d71397aa72128ae6114f23a86f
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2016-10-18 20:04:34 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(array)
   # code here
+  i = 0
+  while i < array.length
+    yield array[i]
+    i += 1
+  end
+  array
+end
+coll = [2,6,3,23]
+my_each(coll) do |e|
 end
