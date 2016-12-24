dcdba8f4738595f6beb02952640f181d68c33293
FATIMA BERAZI <fatimaberaze1997@gmail.com>
2016-12-24 17:07:18 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+ # put argument(s) here
   # code here
-end
+
+  def my_each(collection)
+    i = 0
+    while i < collection.length
+      yield(collection[i])
+      i = i + 1
+    end
+    collection
+  end
