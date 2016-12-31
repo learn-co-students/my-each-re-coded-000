606a68d393d74434a686f23ab0d96416c4279cd2
shadi khaled <shadesoma@gmail.com>
2016-12-31 09:37:59 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
+ # put argument(s) here
   # code here
-end
+  def my_each(array)
+
+    counter=0
+    while counter < array.size
+      yield(array[counter])
+      counter+=1
+    end
+    array
+  end
+  collection = [1, 2, 3, 4]
+  my_each(collection) do |i|
+    puts i
+  end
